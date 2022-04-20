dd888d0e155fb9367642dc054ff121562f5c4bf5
mariiam-aly <xmairam.aly@gmail.com>
2022-04-20 21:57:14 +0200

    perf(language,mode): made language & mode presist

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,19 +1,60 @@
-import React,{useContext} from "react"; 
+import React,{useContext,useEffect, useState} from "react"; 
 import"../styles/Nav.css"
 import { ModeContext } from "../Context/ModeContext";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { LangContext } from "../Context/LangContext";
 function Nav() {
-  const {setMode}= useContext(ModeContext);
-  const { SetLanguage}= useContext(LangContext);
+  const {mode,setMode}= useContext(ModeContext);
+  const {SetLanguage}= useContext(LangContext);
+  const [langSwitch, setLangSwitch]= useState("");
   const { t, i18n } = useTranslation();
 
   const changeLanguage = (language,x,l) => {
     i18n.changeLanguage(language);
-  //document.getElementById("root").style.direction=x;
+    setLangSwitch(language);
+  document.getElementById("root").style.direction=x;
   SetLanguage(l);
   };
+
+  useEffect(()=>{
+
+    const data= window.localStorage.getItem('LANG');
+
+    if (data!==null){
+      i18n.changeLanguage(JSON.parse(data));
+      setLangSwitch(JSON.parse(data));
+    
+   }
+   },[]);
+
+
+  useEffect(()=>{
+
+    window.localStorage.setItem('LANG',JSON.stringify(langSwitch))
+    
+  },[langSwitch]);
+
+  useEffect(()=>{
+
+    const data= window.localStorage.getItem('MODE');
+
+    if (data!==null){
+     
+      setMode(JSON.parse(data));
+    
+   }
+   },[]);
+
+
+  useEffect(()=>{
+
+    window.localStorage.setItem('MODE',JSON.stringify(mode))
+    
+  },[mode]);
+
+  
+  
 
 return(
   <div className="navBar">
@@ -29,7 +70,7 @@ return(
   </div>
 </div>
 
-<select name="modes" id="modes" onChange={(e)=>setMode(e.target.value)}>
+<select name="modes" id="modes" onChange={(e)=>setMode(e.target.value)} value={mode}>
 <option value="light">{t("nav.light")}</option>
 <option value="dark">{t("nav.dark1")}</option>
 <option value="dark2">{t("nav.dark2")}</option> </select>
