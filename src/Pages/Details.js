912f1f064f152c1cf8f2d64eaad0e93574093e10
mariiam-aly <xmairam.aly@gmail.com>
2022-04-20 21:05:53 +0200

    perf(News): persist state

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -6,15 +6,32 @@ import { useTranslation } from "react-i18next";
 function Details() {
   const { t} = useTranslation();
     const [news, setNews]= useState([]);
-    const {number}= useContext(NewsContext);
+    const {number, setNumber}= useContext(NewsContext);
     useEffect( ()=>{
         const getNews =async()=>{
         const response= await Axios.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=152fe400cfec431b8701339385b28ec0");
         setNews(response.data.articles[number]);
+        
   };
 
         getNews();
     },[])
+    useEffect(()=>{
+
+      const data= window.localStorage.getItem('NEWS');
+      console.log(data);
+      if (data!==null){ setNumber(JSON.parse(data));
+      
+     }
+     },[]);
+
+
+    useEffect(()=>{
+
+      window.localStorage.setItem('NEWS',JSON.stringify(number))
+      
+    },[news]);
+
 return(
   <div id="details">
   <p className="title"> {news.author}: {news.title}</p>
