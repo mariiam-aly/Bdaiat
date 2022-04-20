39acc0045de4cb5318f8f8e903ad69b483c3c8cc
mariiam-aly <xmairam.aly@gmail.com>
2022-04-20 15:20:44 +0200

    feat(menu): added settings menu

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,17 +1,30 @@
 import React,{useContext} from "react"; 
 import"../styles/Nav.css"
 import { ModeContext } from "../Context/ModeContext";
-
+import { Link } from "react-router-dom";
 function Nav() {
   const {setMode}= useContext(ModeContext);
 return(
   <div className="navBar">
 <p className="icon">NewsFeed</p>
+<div className="menu">
+<Link to="/">News</Link>
+<div className="dropdown">
+  <button className="dropbtn">Settings</button>
+  <div className="dropdown-content">
+    <a href="#">English</a>
+    <a href="#">Arabic</a>
+  </div>
+</div>
+
 <select name="modes" id="modes" onChange={(e)=>setMode(e.target.value)}>
 <option value="light">Light</option>
 <option value="dark">Dark 1</option>
 <option value="dark2">Dark 2</option> </select>
 
+
+
+</div>
   </div>
 
 
