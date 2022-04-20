ea29c896853ca13a382fb1c19e4a980b1b2e7659
mariiam-aly <xmairam.aly@gmail.com>
2022-04-20 14:03:58 +0200

    feat(route-context): added routes and contexts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
-import './App.css'
+import React, {useState} from "react";
+import './App.scss'
 import {Route} from"react-router-dom"
-
+import Nav from "./Components/Nav";
+import Home from "./Pages/Home";
+import Details from "./Pages/Details";
+import { NewsContext } from "./Context/NewsContext";
+import{ModeContext} from  "./Context/ModeContext";
 function App() {
-    
+   const [number,setNumber] =useState(0);
+   const [mode,setMode] =useState("");
 return(
-  <div>
-
+  <div className={mode}>
+  <ModeContext.Provider value={{mode,setMode}}>
+<Nav/>
+</ModeContext.Provider>
+<NewsContext.Provider value={{number,setNumber}}>
+<Route exact path="/" component={Home} />
+<Route exact path="/det" component={Details} />
+</NewsContext.Provider>
   </div>
 
 
 
 );
 }
+
 
 export default App;
