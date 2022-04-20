b49bec4528a86863e7a4d7b372a9068a63da26bd
mariiam-aly <xmairam.aly@gmail.com>
2022-04-20 19:04:50 +0200

    feat(language-support): added language options

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,25 +2,37 @@ import React,{useContext} from "react";
 import"../styles/Nav.css"
 import { ModeContext } from "../Context/ModeContext";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { LangContext } from "../Context/LangContext";
 function Nav() {
   const {setMode}= useContext(ModeContext);
+  const { SetLanguage}= useContext(LangContext);
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language,x,l) => {
+    i18n.changeLanguage(language);
+  //document.getElementById("root").style.direction=x;
+  SetLanguage(l);
+  };
+
 return(
   <div className="navBar">
 <p className="icon">NewsFeed</p>
 <div className="menu">
-<Link to="/">News</Link>
+<Link to="/">{t("nav.news")}</Link>
 <div className="dropdown">
-  <button className="dropbtn">Settings</button>
+  <button className="dropbtn">{t("nav.settings")}</button>
   <div className="dropdown-content">
-    <a href="#">English</a>
-    <a href="#">Arabic</a>
+  <h6 className="disable" >{t("nav.lang")}</h6>
+    <p onClick={() => changeLanguage("en","ltr",true)}>English</p>
+    <p  onClick={() => changeLanguage("ar","rtl",false)}>العربية</p>
   </div>
 </div>
 
 <select name="modes" id="modes" onChange={(e)=>setMode(e.target.value)}>
-<option value="light">Light</option>
-<option value="dark">Dark 1</option>
-<option value="dark2">Dark 2</option> </select>
+<option value="light">{t("nav.light")}</option>
+<option value="dark">{t("nav.dark1")}</option>
+<option value="dark2">{t("nav.dark2")}</option> </select>
 
 
 
