ead2ad67718bacd04083720946e086f58ccd65ec
mariiam-aly <xmairam.aly@gmail.com>
2022-04-20 19:08:38 +0200

    feat(suspense): added suspense for loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
+import './i18next';
 ReactDOM.render(
   <BrowserRouter>
+  <Suspense fallback={<div>Loading...</div>}>
     <App />
+    </Suspense>
     </BrowserRouter>,
   document.getElementById('root')
 );
