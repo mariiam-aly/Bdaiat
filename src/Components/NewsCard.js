41f4f6d26876692328bb9f48db4d270b5e0c0ea2
mariiam-aly <xmairam.aly@gmail.com>
2022-04-21 18:59:39 +0200

    perf(object): changed method for getting news data

--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -5,12 +5,15 @@ import { NewsContext } from "../Context/NewsContext";
 
 function NewsCard(props) {
     const {setNumber}= useContext(NewsContext);
+
+
+    
 return(
     <div className="col-lg-4 col-md-6">
     <div className="p-3 cardContainer">
 
     <img  className="cardImg" alt={props.title} src={props.image}/>
-   <Link onClick={()=>setNumber(props.index)} style={{textDecoration:"none"}} to="/det"> <div className="cardTitle">{props.title}</div></Link></div>
+   <Link onClick={()=>setNumber({title: props.title, content:props.content, image: props.image, author: props.author, url:props.url, time:props.time})} style={{textDecoration:"none"}} to="/det"> <div className="cardTitle">{props.title}</div></Link></div>
   </div>
 
 
--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -1,31 +1,23 @@
-import React, {useState,useEffect,useContext} from "react"; 
+import React, {useContext,useEffect} from "react"; 
 import"../styles/Details.css"
-import Axios from "axios";
 import { NewsContext } from "../Context/NewsContext";
 import { useTranslation } from "react-i18next";
 function Details() {
   const { t} = useTranslation();
-    const [news, setNews]= useState([]);
-    const {num,setNumber}= useContext(NewsContext);
-    useEffect( ()=>{
-        const getNews =async()=>{
-        const response= await Axios.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=152fe400cfec431b8701339385b28ec0");
-        setNews(response.data.articles[num]);
-        
-  };
+   
+    const {num}= useContext(NewsContext);
 
-        getNews();
-    },[num])
-
-
-  
+    
+    
+    
 return(
   <div id="details">
-  <p className="title"> {news.author}: {news.title}</p>
-  <p className="pub">Publish Date: {news.publishedAt}</p>
-  <img src={news.urlToImage} alt={news.title}/>
- <p className="content">{news.content}</p>
- <a  target="_blank" rel="noreferrer" href={news.url}>{t("det.link")} </a>
+  
+  <p className="title"> {num.author}: {num.title}</p>
+  <p className="pub">Publish Date: { num.time}</p>
+  <img src={num.image} alt={num.title}/>
+ <p className="content">{num.content}</p>
+ <a  target="_blank" rel="noreferrer" href={num.url}>{t("det.link")} </a>
   </div>
 
 
